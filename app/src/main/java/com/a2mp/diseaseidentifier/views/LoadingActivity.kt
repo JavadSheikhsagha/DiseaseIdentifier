b860f950062eaed0b6efea693bba5288df47984d
Javad <sir.hxt@gmail.com>
2023-04-26 16:36:04 +0330

    [FEATURE] implement Loading Screen

--- app/src/main/java/com/a2mp/diseaseidentifier/views/LoadingActivity.kt
+++ app/src/main/java/com/a2mp/diseaseidentifier/views/LoadingActivity.kt
@@ -3,13 +3,22 @@ package com.a2mp.diseaseidentifier.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.a2mp.diseaseidentifier.R
+import com.a2mp.diseaseidentifier.databinding.ActivityLoadingBinding
 
 class LoadingActivity : AppCompatActivity() {
 
+    private lateinit var binding : ActivityLoadingBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loading)
+        binding = ActivityLoadingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupViews()
+
+    }
+
+    private fun setupViews() {
 
 
     }
