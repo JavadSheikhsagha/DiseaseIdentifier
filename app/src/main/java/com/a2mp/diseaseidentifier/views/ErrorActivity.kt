b765613d082744350f440b8c605957e98ca1beb8
Javad <sir.hxt@gmail.com>
2023-04-26 16:33:47 +0330

    [FEATURE] implement Loading Screen

--- app/src/main/java/com/a2mp/diseaseidentifier/views/ErrorActivity.kt
+++ app/src/main/java/com/a2mp/diseaseidentifier/views/ErrorActivity.kt
@@ -1,16 +1,33 @@
 package com.a2mp.diseaseidentifier.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.a2mp.diseaseidentifier.R
+import com.a2mp.diseaseidentifier.databinding.ActivityErrorBinding
 
 class ErrorActivity : AppCompatActivity() {
 
+    lateinit var binding : ActivityErrorBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_error)
+        binding = ActivityErrorBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        setupViews()
 
+    }
+
+    private fun setupViews() {
+
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
+
+        binding.btnTakesnap.setOnClickListener {
+            startActivity(Intent(this, CameraActivity::class.java))
+            finish()
+        }
     }
 }
